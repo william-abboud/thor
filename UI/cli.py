9d0511c4835eb8753090bab5385fb1a7b4c9b763
william-abboud <william_y_abboud@abv.bg>
2021-04-24 16:53:56 +0300

    Made CLI a bit more fancy with colors

--- UI/cli.py
+++ UI/cli.py
@@ -10,14 +10,17 @@ from Preferences.TorrentPreferences import TorrentPreferences
 @click.option("--query", prompt="Enter your search (try being as specific as possible)", help="Specify your query")
 def init(query):
     preferences = TorrentPreferences(5, 'movie', 20)
-    print(f'Searching for "{query}" with a minimum of {preferences.min_num_seeders} seeders...\n')
+    click.secho(f'Searching for "{query}" with a minimum of {preferences.min_num_seeders} seeders...\n', fg='blue')
 
     try:
         best_match = main(query, preferences)
 
-        print(f'Match found! {best_match.name} Check your installed Torrent Client.')
+        click.secho(f'Match found! {best_match.name} Check your installed Torrent Client.', fg='green')
+        click.pause()
     except exceptions.ConnectionError as conn_error:
-        print(conn_error)
+        click.secho(conn_error, fg='red')
     except TorrentMatchNotFoundException as not_found_error:
-        print(not_found_error)
-        print('Hint: If you don\'t find the results you are looking for on the first run try being more specific.\n ')
+        click.secho(not_found_error, fg='red')
+        click.secho(
+            'Hint: If you don\'t find the results you are looking for on the first run try being more specific.\n',
+            fg='yellow')
